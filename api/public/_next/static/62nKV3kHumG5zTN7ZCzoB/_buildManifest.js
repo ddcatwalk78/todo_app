d38a538d0f3b7475327771a810a78201fed0c742
DDCatwalk78 <ddcatwalk78@gmail.com>
2023-06-20 09:21:17 +0900

    https://www.webopixel.net/php/1724.html next.js は App Router を有効にする package.json の build は変更しない next.config.js に output: export, distDir: ../api/public を追加する

---
+++ api/public/_next/static/62nKV3kHumG5zTN7ZCzoB/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-e8b35f8a0cf92802.js"],"/hello":["static/chunks/675-8875249877c8bd96.js","static/chunks/pages/hello-53a1b51d6248f55e.js"],sortedPages:["/_app","/_error","/hello"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- client/next.config.js
+++ client/next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: 'export',
+  distDir: '../api/public',
+}
 
 module.exports = nextConfig
